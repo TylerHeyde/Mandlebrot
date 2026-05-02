bf4d6d39a209be5c56b8f4f387b27c8caa61b4ba
Tyler <tylerheyde1@gmail.com>
2026-05-01 18:59:18 -0700

    Project is Complete, now awaiting extra credit addditions

--- Code/ComplexPlane.cpp
+++ Code/ComplexPlane.cpp
@@ -1,17 +1,18 @@
 #include "ComplexPlane.h"
 //---public functions---//
 //constructor
+
 ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight) {
 	m_pixel_size = { pixelWidth, pixelHeight };
 	m_aspectRatio = (float)sf::VideoMode::getDesktopMode().height / (float)sf::VideoMode::getDesktopMode().width;
-	m_plane_center = {0, 0};
+	m_plane_center = { 0, 0 };
 	m_zoomCount = 0;
 	m_State = State::CALCULATING;
 	m_vArray.setPrimitiveType(sf::Points);
 	m_vArray.resize(pixelWidth * pixelHeight);
 }
 
-void ComplexPlane::draw(sf::RenderTarget& target, sf::RenderStates states) const{
+void ComplexPlane::draw(sf::RenderTarget& target, sf::RenderStates states) const {
 	target.draw(m_vArray, states);
 }
 
@@ -37,31 +38,55 @@ void ComplexPlane::updateRender() {
 	}
 }
 
-void ComplexPlane::zoomIn(){
-
+void ComplexPlane::zoomIn() {
+	m_zoomCount++;
+	float x_size = BASE_WIDTH * pow(BASE_ZOOM, m_zoomCount);
+	float y_size = BASE_HEIGHT * m_aspectRatio * pow(BASE_ZOOM, m_zoomCount);
+	m_plane_size = { x_size, y_size };
+	m_State = CALCULATING;
 }
 
-void ComplexPlane::zoomOut(){
-
+void ComplexPlane::zoomOut() {
+	m_zoomCount--;
+	float x_size = BASE_WIDTH * pow(BASE_ZOOM, m_zoomCount);
+	float y_size = BASE_HEIGHT * m_aspectRatio * pow(BASE_ZOOM, m_zoomCount);
+	m_plane_size = { x_size, y_size };
+	m_State = CALCULATING;
 }
 
-void ComplexPlane::setCenter(sf::Vector2i mousePixel){
-
+void ComplexPlane::setCenter(sf::Vector2i mousePixel) {
+	m_plane_center = ComplexPlane::mapPixelToCoords(mousePixel);
 }
 
-void ComplexPlane::setMouseLocation(sf::Vector2i mousePixel){
-
+void ComplexPlane::setMouseLocation(sf::Vector2i mousePixel) {
+	//sf::Mouse::getPosition(window);	// relative to the window
+	// sf::Mouse::getPosition()	// global screen position
+	// window.mapPixelToCoords(sf::Mouse::getPosition(window))	// as it floats
+	m_mouseLocation = mapPixelToCoords(mousePixel);
 }
 
-void ComplexPlane::loadText(sf::Text& text){
+void ComplexPlane::loadText(sf::Text& text) {
+	stringstream ss;
 
+	ss << fixed << setprecision(2);
+
+	ss << "Center: (" << m_plane_center.x << ", " << m_plane_center.y << ")\n";
+
+	ss << "Cursor: (" << m_mouseLocation.x << ", " << m_mouseLocation.y << ")\n";
+
+	ss << "Left-click to Zoom in" << "\n";
+	ss << "Right-click to Zoom out" << "\n";
+
+	text.setString(ss.str());
 }
 
 //---private functions---//
 int ComplexPlane::countIterations(sf::Vector2f coord) {
 	int iterations = 0;
+	//---this is the code that was written based on the assignment description---//
+
 	complex<double> c = { coord.x, coord.y };
-	complex<double> z = (0,0);
+	complex<double> z = (0, 0);
 
 	//was doing some research and found that using norm() is faster than abs() for checking the magnitude of a complex number, so I switched to that
 	while (norm(z) <= 4.0 && iterations < MAX_ITER) {
@@ -69,18 +94,48 @@ int ComplexPlane::countIterations(sf::Vector2f coord) {
 		++iterations;
 	}
 
+
+	//went gpt ways to speed up exicution and it said doing it manually is faster than using the complex library, so this is just trying that
+
+	//double zx = 0.0, zy = 0.0;
+	//double cx = coord.x, cy = coord.y;
+
+	//while (zx * zx + zy * zy <= 4.0 && iterations < MAX_ITER) {
+	//	double xtemp = zx * zx - zy * zy + cx;
+	//	zy = 2.0 * zx * zy + cy;
+	//	zx = xtemp;
+	//	++iterations;
+	//}
+
 	return iterations;
 }
 
 void ComplexPlane::iterationsToRGB(size_t count, uint8_t& r, uint8_t& g, uint8_t& b) {
+	if (count >= MAX_ITER) {
+		r = 0;
+		g = 0;
+		b = 0;
+		return;
+	}
 	//  Map your iteration count to a value t between[0:1]
 	//	red(t) = startRed * (1 - t) + endRed * t
 	//	green(t) = startGreen * (1 - t) + endGreen * t
 	//	blue(t) = startBlue * (1 - t) + endBlue * t
 	float t = (float)count / (float)MAX_ITER;
-	r = 255 * (1 - t) + 0 * t;
-	g = 255 * (1 - t) + 0 * t;
-	b = 255 * (1 - t) + 0 * t;
+	//r = 0 * (1 - t) + 255 * t;
+	//g = 0 * (1 - t) + 255 * t;
+	//b = 0 * (1 - t) + 255 * t;
+	//r = sin(3.0 * t + 0.0) * 127 + 128;
+	//g = sin(3.0 * t + 2.0) * 127 + 128;
+	//b = sin(3.0 * t + 4.0) * 127 + 128;
+	//r = (uint8_t)(255 * t);
+	//g = (uint8_t)(255 * t);
+	//b = (uint8_t)(255 * t);
+
+	//this is the clasic mandelbrot coloring
+	r = (uint8_t)(9 * (1 - t) * t * t * t * 255);
+	g = (uint8_t)(15 * (1 - t) * (1 - t) * t * t * 255);
+	b = (uint8_t)(8.5 * (1 - t) * (1 - t) * (1 - t) * t * 255);
 }
 
 //maps the pixel coordinates [a,b] which is the [x,y] position of the pixel to [c,d] which is the [x,y] position on the complex plane
--- Code/Mandelbrot.cpp
+++ Code/Mandelbrot.cpp
@@ -1,79 +1,97 @@
 #include <SFML/Graphics.hpp>
-
 #include <iostream>
-
-
-
+#include "ComplexPlane.h"
 
 using namespace sf;
-
 using namespace std;
-
-
 
 
 int main()
 
 {
 
-	int pixelWidth = VideoMode::getDesktopMode().width;
+    int pixelWidth = VideoMode::getDesktopMode().width;
 
-	int pixelHeight = VideoMode::getDesktopMode().height;
+    int pixelHeight = VideoMode::getDesktopMode().height;
 
-	VideoMode vm(pixelWidth, pixelHeight);
-	// Create the main redering window 
-	RenderWindow window(vm, "Mandlebrot", Style::Default);
+    VideoMode vm(pixelWidth, pixelHeight);
+    // Create the main redering window 
+    RenderWindow window(vm, "Mandlebrot", Style::Default);
+    ComplexPlane cPlane(pixelWidth, pixelHeight);
 
-	// debug code for testing rendering 
-	/*
-	VertexArray particle1(TriangleFan);
-	particle1.resize(5);
+    sf::Font Arial;
+    Arial.loadFromFile("Fonts\\Arial Types\\ARIAL.ttf");
 
-	Vector2f center = { (float)pixelWidth / 2.0f, (float)pixelHeight / 2.0f };
+    sf::Text info;
+    info.setFont(Arial);
+    info.setCharacterSize(10);
+    info.setFillColor(sf::Color::White);
+    info.setPosition(5.0f, 5.0f);
 
-	particle1[0].color = Color::Blue;
-	particle1[0].position = center;
-	particle1[1].color = Color::Green;
-	particle1[1].position = { center.x - 100.0f, center.y - 100.0f };
-	particle1[2].color = Color::Red;
-	particle1[2].position = { center.x + 100.0f, center.y - 100.0f };
-	particle1[3].color = Color::Red;
-	particle1[3].position = { center.x + 100.0f, center.y + 100.0f };
-	particle1[4].color = Color::Red;
-	particle1[4].position = { center.x - 100.0f, center.y + 100.0f };
-	*/
-
-	while (window.isOpen())
-
-	{
-
-		///Input
-
-		if (Keyboard::isKeyPressed(Keyboard::Escape))
-
-		{
-
-			window.close();
-
-		}
+    // debug code for testing rendering 
+    // VertexArray particle1(TriangleFan);
+    // particle1.resize(5); 
+    // Vector2f center = { (float)pixelWidth / 2.0f, (float)pixelHeight / 2.0f };
+    // particle1[0].color = Color::Blue;
+    // particle1[0].position = center;
+    // particle1[1].color = Color::Green;
+    // particle1[1].position = { center.x - 100.0f, center.y - 100.0f };
+    // particle1[2].color = Color::Red;
+    // particle1[2].position = { center.x + 100.0f, center.y - 100.0f };
+    // particle1[3].color = Color::Red;
+    // particle1[3].position = { center.x + 100.0f, center.y + 100.0f };
+    // particle1[4].color = Color::Red;
+    // particle1[4].position = { center.x - 100.0f, center.y + 100.0f };
 
 
+    while (window.isOpen())
+    {
+        ///Input
+        if (Keyboard::isKeyPressed(Keyboard::Escape))
+        {
+            window.close();
+        }
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::MouseButtonPressed)
+            {
+                if (event.mouseButton.button == sf::Mouse::Right)
+                {
+                    cout << "zoomOut()\n";
+                    cPlane.zoomOut();
+                    cPlane.setCenter({ event.mouseButton.x, event.mouseButton.y });
+                }
+                else if (event.mouseButton.button == sf::Mouse::Left)
+                {
+                    cout << "zoomIn()\n";
+                    cPlane.zoomIn();
+                    cPlane.setCenter({ event.mouseButton.x, event.mouseButton.y });
+                }
+            }
+            if (event.type == sf::Event::MouseMoved)
+            {
+                cPlane.setMouseLocation(sf::Mouse::getPosition());
+                cout << "setMouseLocation\n";
+            }
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+        ///Update
+        cPlane.updateRender();
+        cPlane.loadText(info);
 
+        ///Draw
+        window.clear();
+        window.draw(cPlane);
+        // cPlane.draw(window, RenderStates::Default);
+        window.draw(info);
 
-		///Update
+        window.display();
+    }
 
-
-
-
-		///Draw
-
-		window.clear();
-
-		/*Debug*/  //window.draw(particle1);
-
-		window.display();
-
-	}
 
 
 
